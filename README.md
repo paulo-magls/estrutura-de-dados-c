2f4b75ef2768420c6dc56aa5b987728f7701a77f
paulo-magls <paulomaga225@gmail.com>
2023-03-08 18:41:15 -0300

    implementando a pilha

---
+++ README.md
@@ -0,0 +1,1 @@
+# estrutura-de-dados-c
---
+++ pilha/pilha.h
@@ -0,0 +1,55 @@
+// pilha.h - implementação dinâmica sequêncial de pilha
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Itemp;
+
+typedef struct pilha {
+   int    max;
+   int    topo;
+   Itemp *item;
+} *Pilha;
+
+Pilha pilha(int m) {
+   Pilha P = (Pilha) malloc(sizeof(struct pilha));
+   P->max  = m;
+   P->topo = -1;
+   P->item = (Itemp *) malloc(m*sizeof(Itemp));
+   return P;
+}
+
+int vaziap(Pilha P) {
+   if( P->topo == -1 ) return 1;
+   else return 0;
+}
+
+int cheiap(Pilha P) {
+   if( P->topo == P->max-1 ) return 1;
+   else return 0;
+}
+
+void empilha(Itemp x, Pilha P) {
+   if( cheiap(P) ) { puts("pilha cheia!"); abort(); }
+   P->topo++;
+   P->item[P->topo] = x;
+}
+
+Itemp desempilha(Pilha P) {
+   if( vaziap(P) ) { puts("pilha vazia!"); abort(); }
+   Itemp x = P->item[P->topo];
+   P->topo--;
+   return x;
+}
+
+Itemp topo(Pilha P) {
+   if( vaziap(P) ) { puts("pilha vazia!"); abort(); }
+   return P->item[P->topo];
+}
+
+void destroip(Pilha *P) {
+   free((*P)->item);
+   free(*P);
+   *P = NULL;
+}
+
