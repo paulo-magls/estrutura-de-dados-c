3b1675c5ed694f095e9db2635ba792a3a7ac80c5
paulo-magls <paulomaga225@gmail.com>
2023-03-10 19:01:05 -0300

    implementando um conversor de expressão infixa para posfixa

---
+++ pilha/posfixa.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+// função isdigit(): isdigit(5) -> 1 || isdigit('A') -> 0
+#include <string.h>
+// função strchr(): strchr("ABC", 'A') -> 1 || strchr("ABC", 'D') -> 0
+#include "pilha.h"
+
+char *posfixa(char *e) {
+	static char s[256];
+	int j = 0;
+	Pilha p = pilha(256);
+	
+	for(int i=0; e[i]; i++)
+		if(isdigit(e[i])) s[j++] = e[i];
+		else if(strchr("+-*/", e[i])) empilha(e[i], p);
+		else if(e[i] == ')') s[j++] = desempilha(p);
+	
+	s[j] = '\0';
+	destroip(&p);
+	return s;
+}
+
+int main(void) {
+	char e[256];
+	
+	printf("Infixa? "); 
+	gets(e); // Exemplo de entrada: ((2*3)+(8/4))
+	
+	printf("Posfixa = %s\n", posfixa(e)); // Saída: 23*84/+
+	return 0;
+}
